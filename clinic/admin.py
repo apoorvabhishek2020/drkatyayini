42a383f51514106636f9a0754f6e37cafe37fe02
apoorvabhishek2020 <apoorvabhishek2020@gmail.com>
2022-10-21 15:37:04 +0530

    added search bar

--- clinic/admin.py
+++ clinic/admin.py
@@ -3,10 +3,12 @@ from .models import Patient, Medicine, PersonalHistory
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
     list_display= ['name', 'age', 'address', 'date_of_examination']
+    search_fields = ['name', 'age', 'address', 'date_of_examination']
 
 @admin.register(Medicine)
 class MedicineAdmin(admin.ModelAdmin):
     list_display= ['name', 'composition']
+    search_fields = ['name', 'composition']
 
 @admin.register(PersonalHistory)
 class PersonalHistoryAdmin(admin.ModelAdmin):
