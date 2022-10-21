779034fb261bfa083cb272715b586a8b81a140b0
apoorvabhishek2020 <apoorvabhishek2020@gmail.com>
2022-10-21 15:30:39 +0530

    version 2.0

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Patient, Medicine
+from .models import Patient, Medicine, PersonalHistory
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
-    list_display= ['name', 'age', 'sex', 'submission_date']
+    list_display= ['name', 'age', 'address', 'date_of_examination']
 
 @admin.register(Medicine)
 class MedicineAdmin(admin.ModelAdmin):
     list_display= ['name', 'composition']
+
+@admin.register(PersonalHistory)
+class PersonalHistoryAdmin(admin.ModelAdmin):
+    list_display= ['name']
